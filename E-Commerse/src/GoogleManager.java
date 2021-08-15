f66ee707344bb273289817b0e595733d24bc0ebc
atacan <atacanerdoan@gmail.com>
2021-08-15 04:39:00 +0300

    Delete GoogleManager.java

--- E-Commerse/src/GoogleManager.java
+++
@@ -1,7 +0,0 @@
-
-public class GoogleManager {
-	public void registerWithGoogleAccount(User user) {
-		System.out.println("google ile oturum açıldı : " + user.getFirstName() + " " + user.getLastName());
-	}
-
-}
